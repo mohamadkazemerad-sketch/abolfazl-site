d6c5954983599756e5b73b82047f9e08c5bfe717
mohamadkazemerad-sketch <mohamadkazemerad@gmail.com>
2026-06-21 11:39:57 +0330

    Update admin.js

--- admin.js
+++ admin.js
@@ -2,22 +2,52 @@ let requests = JSON.parse(localStorage.getItem("requests")) || [];
 
 let box = document.querySelector("#requests");
 
-requests.forEach(function(item){
+let count = document.querySelector("#count");
+
+
+count.innerHTML = 
+"تعداد درخواست‌ها: " + requests.length;
+
+
+requests.forEach(function(item,index){
 
     box.innerHTML += `
-    
+
     <div class="request-card">
 
         <h3>${item.name}</h3>
 
-        <p>شماره: ${item.phone}</p>
+        <p>📞 شماره: ${item.phone}</p>
 
-        <p>خدمات: ${item.service}</p>
+        <p>🔧 خدمات: ${item.service}</p>
 
         <p>${item.message}</p>
+
+
+        <button onclick="deleteRequest(${index})">
+        حذف درخواست
+        </button>
+
 
     </div>
 
     `;
 
 });
+
+
+
+function deleteRequest(index){
+
+    requests.splice(index,1);
+
+
+    localStorage.setItem(
+        "requests",
+        JSON.stringify(requests)
+    );
+
+
+    location.reload();
+
+}
